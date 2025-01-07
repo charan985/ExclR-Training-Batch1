db728120514c0a327d579998eefa2d8205f6c4d9
charan985 <charan.8259@gmail.com>
2025-01-07 13:19:09 +0530

    Add files via upload

---
+++ Demo013.java
@@ -0,0 +1,9 @@
+public class Demo013 {
+    public static void main(String[] args) {
+        int base = 10;
+        int height = 20;
+        double area = 0.5*base*height;
+        System.out.println(area);
+
+    }
+}
